03ada66d1f01f14acfc52121954f0bab37c402d0
Paulo Mendonca <paulovictorsampaio@hotmail.com>
2024-01-19 23:12:36 -0300

    fix: float digits

--- Projeto 01 - Sistema Bancário/desafio.py
+++ Projeto 01 - Sistema Bancário/desafio.py
@@ -19,7 +19,7 @@ while True:
         valor = float(input("Informe o valor do depósito: "))
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor :. 2f}\n"
+            extrato += f"Depósito: R$ {valor:.2f}\n"
         else:
             print("Operação falhou! O valor informado é inválido.")
     elif opcao == "s":
@@ -35,14 +35,14 @@ while True:
             print("Operação falhou! Número máximo de saques excedido.")
         elif valor > 0:
             saldo -= valor
-            extrato += f"Saque: R$ {valor : .2f}\n"
+            extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
         else:
             print("Operação falhou! O valor informado é inválido.")
     elif opcao == "e":
         print("\n======EXTRATO======")
         print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo :. 2f}")
+        print(f"\nSaldo: R$ {saldo:.2f}")
         print("======================")
     elif opcao == "q":
         break
